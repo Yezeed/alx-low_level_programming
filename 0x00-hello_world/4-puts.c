4c2262726de8cd3d0c066b93f9327a4ce3236bfe
Yezeed <yexxy94@yahoo.com>
2021-08-27 03:33:30 -0700

    initial commit

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-/*
+/**
 * main - prints "Programming is like a multilingual puzzle"
 * follwed by a new line.
 * Return: always 0.
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-  puts("Programming is like building a multilingual puzzle\n");
-  return(0);
+	puts("Programming is like building a multilingual puzzle\n");
+	return(0);
 }
 
