25b36db699c693091caaf8c6d7dc39b44053c8b1
Yezeed <yexxy94@yahoo.com>
2021-08-27 03:22:56 -0700

    C program that prints with puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+* main - prints "Programming is like a multilingual puzzle"
+* follwed by a new line.
+* Return: always 0.
+*/
+
+int main(void)
+{
+  puts("Programming is like building a multilingual puzzle\n");
+  return(0);
+}
+
