cde8f11cad809f5f49bc8be1271bde6f04ce5e62
Yezeed <yexxy94@yahoo.com>
2021-08-30 05:56:51 -0700

    commit

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - main block
+* Description: Print all digits in base 10 from '0' using putchar.
+* Return: Zer0
+*/
+
+int main(void)
+{
+	int i;
+
+	while (i == 0 && i < 10)
+	{
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
