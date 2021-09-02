beda1280cc31beb936dd1976c54a3e124e12a9fd
Yezeed <yexxy94@yahoo.com>
2021-09-02 06:23:32 -0700

    commit

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,13 +1,12 @@
 #include "main.h"
 
 /**
- * main - print_alphabet_x10: Print alphabet 10 times.
+ * print_alphabet_x10: Print alphabet 10 times.
  * Output will be in lowercase.
  * Return: void
  */
 
-void print_alphabet_x10(void);
-int main(void)
+void print_alphabet_x10(void)
 {
 	int i = 0;
 	char j;
