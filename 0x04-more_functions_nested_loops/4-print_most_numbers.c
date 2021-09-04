f384245997ef35d7bdec97df133f6213b406cc4b
Yezeed <yexxy94@yahoo.com>
2021-09-04 14:32:51 -0700

    commit

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function prints numbers from 0-9
+ * 2 and 4 are not printed, using '_putchar' only twice.
+ * Return: void.
+ */
+
+void print_most_numbers(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		if (i != 2 && i != 4)
+			_putchar((i % 10) + '0');
+	}
+	i++;
+
+	_putchar('\n');
+}
