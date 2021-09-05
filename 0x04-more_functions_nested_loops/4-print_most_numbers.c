e179cdc3b7cdb4879acf64c554895bd73819653d
Yezeed <yexxy94@yahoo.com>
2021-09-05 10:28:16 -0700

    commit

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,14 +8,13 @@
 
 void print_most_numbers(void)
 {
-	int i = 0;
+	int i;
 
-	while (i <= 9)
+	for (i = 0; i <= 9; i++)
 	{
 		if (i != 2 && i != 4)
 			_putchar((i % 10) + '0');
 	}
-	i++;
 
 	_putchar('\n');
 }
