c45eadab90115a3455f4fd3f0b97096f9c395811
Yezeed <yexxy94@yahoo.com>
2021-09-07 12:18:09 -0700

    Function reset int n to 98

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,9 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  * reset_to_98 - takes a pointer to an int as parameter
  * And updates the value it point to to 98.
  * @*n - the pointer to be taken to an int n.
- * @n - the variable to be reset to 98.
  * Return: voind
  */
 
