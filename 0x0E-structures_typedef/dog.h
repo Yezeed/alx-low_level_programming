d858e27ad8be4fd2279b50222e6740e0e30234c2
Yezeed <yexxy94@yahoo.com>
2021-10-04 08:00:37 -0700

    Header file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,10 @@ typedef struct dog
 	float age;
 } dog_t;
 
+int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /* #ifndef DOH_H */
