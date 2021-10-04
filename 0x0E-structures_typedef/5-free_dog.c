1ce5cff9990b289b7ada6a46180b7a476eaf4198
Yezeed <yexxy94@yahoo.com>
2021-10-04 09:05:48 -0700

    Free dogs

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,14 +1,14 @@
 #include "dog.h"
 #include <stdlib.h>
 
-/*8
+/**
  * free_dog - function frees dog
  * @d: freed dog
  */
 
 void free_dog(dog_t *d)
 {
-	if (d!= NULL)
+	if (d != NULL)
 	{
 		free(d->name);
 		free(d->owner);
