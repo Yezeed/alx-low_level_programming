1fe1be8a6ec46b485678d00db840bd1591bad577
Yezeed <yexxy94@yahoo.com>
2021-10-04 08:11:51 -0700

    Create a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -41,24 +41,19 @@ char *_strdup(char *str)
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
-	
 	new_dog = malloc(sizeof(struct dog));
 	if (new_dog == NULL)
 		return (NULL);
-	
 	new_dog->name = name;
 	new_dog->name = _strdup(name);
-	
 	if (new_dog->name == NULL)
 	{
 		free(new_dog);
 		return (NULL);
 	}
-	
 	new_dog->age = age;
 	new_dog->owner = owner;
 	new_dog->owner = _strdup(owner);
-	
 	if (new_dog->owner == NULL)
 	{
 		free(new_dog);
