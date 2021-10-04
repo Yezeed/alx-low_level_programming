da64ec37b8e2cd2c071e35669b6016a721eb6245
Yezeed <yexxy94@yahoo.com>
2021-10-04 05:45:54 -0700

    Define a new type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure of a dog
+ * @name: dog's name
+ * @age: dog's name
+ * @owner: dog's owner
+ */
+
+typedef struct dog
+{
+	char *name, *owner;
+	float age;
+} dog_t;
+
+#endif /* #ifndef DOH_H */
